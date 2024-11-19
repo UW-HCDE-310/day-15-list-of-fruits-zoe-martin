255a0d5786c35823a90e54bdd4eeb4173c3ee43b
Zoe Martin <zoemartin@Zoes-MacBook-Air-10.local>
2024-11-19 11:10:59 -0800

    added secret keys

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+import keys
+
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -11,4 +13,4 @@ def index():
     o_fruits = [fruit for fruit in fruits if fruit["name"].startswith("o")]
     less_than_three_fruits = [fruit for fruit in fruits if fruit["quantity"] < 3]
 
-    return render_template("index.html", fruits=o_fruits)
+    return render_template("index.html", fruits=o_fruits, key_1=keys.MY_SECRET_API_KEY_1, key_2=keys.MY_SECRET_API_KEY_2)
